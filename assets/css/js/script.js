84250adcf2412caff874fea728ffb59d94b307d5
l5-cryptohhhh <nunziatamanuel5@gmail.com>
2026-05-13 17:36:20 +0200

    D3W2

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -7,8 +7,25 @@ REGOLE
 */
 
 const starWarsCharacters = [
-    { name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", eye_color: "blue", birth_year: "19BBY", gender: "male" },
-    { name: "C-3PO", height: "167", mass: "75", hair_color: "n/a", skin_color: "gold", eye_color: "yellow", birth_year: "112BBY", gender: "n/a" },
+    {
+        name: "Luke Skywalker",
+        height: "172", mass: "77",
+        hair_color: "blond",
+        skin_color: "fair",
+        eye_color: "blue",
+        birth_year: "19BBY",
+        gender: "male"
+    },
+    {
+        name: "C-3PO",
+        height: "167",
+        mass: "75",
+        hair_color: "n/a",
+        skin_color: "gold",
+        eye_color: "yellow",
+        birth_year: "112BBY",
+        gender: "n/a"
+    },
     { name: "R2-D2", height: "96", mass: "32", hair_color: "n/a", skin_color: "white, blue", eye_color: "red", birth_year: "33BBY", gender: "n/a" },
     { name: "Darth Vader", height: "202", mass: "136", hair_color: "none", skin_color: "white", eye_color: "yellow", birth_year: "41.9BBY", gender: "male" },
     { name: "Leia Organa", height: "150", mass: "49", hair_color: "brown", skin_color: "light", eye_color: "brown", birth_year: "19BBY", gender: "female" },
@@ -31,7 +48,7 @@ const nomi = [];
 for (let i = 0; i < starWarsCharacters.length; i++) {
     nomi.push(starWarsCharacters[i].name);
 }
-console.log(nomi);
+console.table(nomi);
 
 
 /* ESERCIZIO 2 — Solo i femminili
@@ -98,7 +115,7 @@ for (let i = 0; i < starWarsCharacters.length; i++) {
     }
 };
 
-console.log(perColoriOcchi);
+console.table(perColoriOcchi);
 
 
 /* ESERCIZIO 5 — Massa totale dell'equipaggio (while)
